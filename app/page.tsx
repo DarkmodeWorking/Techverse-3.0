bee2760944e917f806d169c66147c74f704cd415
Nanda Lal Das <tikludas01@gmail.com>
2024-09-04 22:28:39 +0530

    adeed the Events section structure yet to customize it also modified next.config.mjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Earth } from "@/components/Earth";
+import { Events } from "@/components/Events";
 import { Hero } from "@/components/Hero";
-import Image from "next/image";
+import Image from "next/image"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero/>
       <Earth/>
+      <Events/>
     </main>
   );
 }
