c3faaf7e2cd27cbf65ae97293ddb5ffe3e840ac0
Nanda Lal Das <tikludas01@gmail.com>
2024-09-04 17:05:35 +0530

    added the globe, now the timer needs to be added above the globe

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Earth } from "@/components/Earth";
 import { Hero } from "@/components/Hero";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero/>
+      <Earth/>
     </main>
   );
 }
