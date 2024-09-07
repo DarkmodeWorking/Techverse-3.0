6a9501bf84f29fa91d5a7dc0638b93b3c6db88b2
Anurag <hamsen3880@gmail.com>
2024-09-07 20:40:46 +0530

    Fixes to Core Team and Navbar addition

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,15 +2,19 @@
 
 import { AboutPageHero } from "@/components/AboutPageHero";
 import { CoreTeam } from "@/components/CoreTeam";
+import { NavbarDemo } from "@/components/NavbarDemo";
 import { StarBackground } from "@/components/StarBackground";
 import { div } from "framer-motion/client";
 
 export default function AboutPage() {
     return (
-        <div>
+      <div>
+        <NavbarDemo />
+        <div className="mt-10 flex min-h-screen flex-col items-center justify-between">
        <AboutPageHero/>
-       <StarBackground/>
+       {/* <StarBackground/> */}
        <CoreTeam/>
+      </div>
       </div>
     );
   }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import { Earth } from "@/components/Earth";
 import { Events } from "@/components/Events";
 import { Hero } from "@/components/Hero";
+import { NavbarDemo } from "@/components/NavbarDemo";
 import Image from "next/image"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <div>
+      <NavbarDemo />
+    <main className="mt-40 flex min-h-screen flex-col items-center justify-between">
+      
       <Hero/>
       <Earth/>
       <Events/>
     </main>
+    </div>
   );
 }
