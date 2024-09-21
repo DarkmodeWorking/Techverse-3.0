ca5f328edb9422693b5f5238015ab44c26fe9ca8
Nanda Lal Das <tikludas01@gmail.com>
2024-09-21 14:27:58 +0530

    changed fonts

--- components/Events.tsx
+++ components/Events.tsx
@@ -47,52 +47,13 @@ export function Events() {
 
   return (
     <div className="w-full h-full pt-20" id="events">
-      <h2 className="text-center max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-        Events
+      <h2 style={{ fontFamily: 'CustomFont' }} className="text-center max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+        EVENTS
       </h2>
       <Carousel items={cards} />
     </div>
   );
 }
-
-// const DummyContent = () => {
-//   return (
-//     <div
-//       key={"dummy-content"}
-//       className="bg-[#F5F5F7] dark:bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
-//     >
-//       <p className="text-neutral-600 pb-10 dark:text-neutral-400 text-base md:text-xl font-sans max-w-3xl mx-auto">
-//         <span className="font-bold text-neutral-700 dark:text-neutral-200">
-          
-//         </span>{" "}
-//         Oi Cunt solve this Linked List or I shall put Omlandah on yo ass
-//       </p>
-//       <Image
-//         src="/images/supercoders.jpeg"
-//         alt="Macbook mockup from Aceternity UI"
-//         width={500}
-//         height={500}
-//         className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
-//       />
-//       <p className="text-neutral-600 pb-10 dark:text-neutral-400 text-base md:text-xl font-sans max-w-3xl mx-auto">
-//         Solo: ₹ 69
-//         Duo: ₹ 420
-//       </p>
-//       <div className="flex justify-between">
-//         <p className="text-neutral-600 pb-10 dark:text-neutral-400 text-base md:text-xl font-sans max-w-3xl mx-auto">
-//           Event Head: Onurag Cunt <br />
-//           Phone: +91 69420 69420 <br />
-//           Email: omlandahdonkillmywife@gmail.com
-//         </p>
-//         <p className="text-neutral-600 pb-10 dark:text-neutral-400 text-base md:text-xl font-sans max-w-3xl mx-auto">
-//           Co-Event Head: Omlandah <br />
-//           Phone: +91 42069 42069 <br />
-//           Email: tookmybloodyson@reddiffmail.com
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
 
 const data = [
   {
