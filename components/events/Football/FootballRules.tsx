8cede163e4593746baa2a7c826b80ed4f60061bb
Nanda Lal Das <tikludas01@gmail.com>
2024-10-03 20:00:45 +0530

    changed E-football details

--- components/events/Football/FootballRules.tsx
+++ components/events/Football/FootballRules.tsx
@@ -1,52 +1,48 @@
 export function FootballRules() {
-    return (
-      <div
-        key={"dummy-content"}
-        className="bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
-      >
-        <p className=" pb-10 text-neutral-400 text-xs md:text-xl font-sans max-w-3xl mx-auto">
-          <b> MATCH TIME: </b> <br /><br />
-          <ul>
-            <li>KNOCKOUT STAGE - 7 MINS (ET + PK ON)</li>
-            <li>SEMI-FINAL - 8 MINS (ET + PK ON) </li>
-            <li>FINAL - 10 MINS (ET + PK ON)</li>
-          </ul>
-          <br />
-          <b>TOURNAMENT FORMAT: </b><br /><br />
-          <ul>
-            <li>SLOTS LIMIT: 32 TEAMS</li>
-            <li>KNOCKOUT STAGE: TEAMS WILL BE DRAWN UP RANDOMLY</li>
-          </ul>
-          <br />
-          <b>TOURNAMENT RULES AND REGULATIONS: </b> <br /><br />
-          <ol>
-            <li>1. TOURNAMENT TYPE: 3 V 3 (+1 SUB).</li><br />
-            <li>2. ALL CARDS ARE ALLOWED.</li><br />
-            <li>
-              3. DURING MATCH, IF ANYONE USES FOUL LANGUAGE, THE INDIVIDUAL WILL BE SHOWN A YELLOW CARD. 
-              2 YELLOW CARDS MEAN A RED CARD, WHICH WILL RESULT IN SUSPENSION FOR 1 MATCH.
-            </li><br />
-            <li>4. EXTERNAL ACCESSORIES LIKE JOYSTICK OR TRIGGER ARE NOT ALLOWED.</li><br />
-            <li> 
-              5. IF ANYONE FACES LAG ISSUES OR NETWORK ISSUES DURING THE MATCH AND GETS DISCONNECTED, 
-              THEN A REMATCH WITH REMAINING TIME WOULD BE ARRANGED, KEEPING THE PREVIOUS SCORE IN RECORD.
-            </li> <br />
-            <li>6. THE REGISTRATION FEE IS NON-REFUNDABLE.</li> <br />
-            <li>7. NO ONLINE PARTICIPATION IS ALLOWED. THIS IS A LAN TOURNAMENT.</li> <br />
-            <li>
-              8. IF A MATCH IS TIED, THEN BEST VS BEST WILL BE PLAYED.
-            </li> <br />
-            <li>9. IPADS OR ANY SORT OF TABLETS ARE NOT ALLOWED.</li><br />
+  return (
+    <div
+      key={"dummy-content"}
+      className="bg-neutral-800 p-8 md:p-14 rounded-3xl mb-4"
+    >
+      <div className="pb-10 text-neutral-400 text-xs md:text-xl font-sans max-w-3xl mx-auto">
+        <b>RULES & REGULATIONS</b> <br /><br />
 
-            <li>
-              10. ALL TEAMS AND PARTICIPANTS MUST ARRIVE ON TIME. IF ANY TEAM DOES NOT REPORT AT THE 
-              REPORTING TIME / MATCH TIME, THEN THE OPPONENT TEAM WILL GET A WALKOVER.
-            </li> <br />
-            <li>11. THE DECISION OF EVENT HEADS WILL BE FINAL.</li><br />
-          </ol>
-        </p>
-          
+        <ul>
+          <li><b>Total number of teams:</b> 32</li>
+          <li><b>Tournament type:</b> Group + Knockout Team Tournament</li>
+          <li><b>In match rules:</b></li>
+          <ul>
+            <li><b>Total number of matches in a knockout round:</b> Single leg</li>
+            <li><b>Match length:</b> 6 minutes</li>
+            <li><b>ET & PK:</b> Off</li>
+            <li><b>Number of sub intervals:</b> 3</li>
+            <li><b>Number of subs:</b> 6</li>
+            <li><b>Condition (Home & Away):</b> Random</li>
+            <li><b>Injuries:</b> On</li>
+          </ul>
+        </ul>
+        <br />
 
-        </div>
-    );
-  };
+        <b>RULES</b> <br /><br />
+
+        <ol>
+          <li>32 teams participating in this tournament. Group stage draw of 32 teams will be held one week before the tournament live on Instagram.</li><br />
+          <li>Each team needs to arrive at the venue by 10:00 AM.</li><br />
+          <li>If any team is not present for their match within 20 minutes, a walkover will be provided to their opponent.</li><br />
+          <li>The fixtures will be provided to the BPF Instagram account after the group stage draw.</li><br />
+          <li>32 teams will be divided into 8 groups. Top 2 teams from each group will qualify for the knockout round based on points and GD (goal difference).</li><br />
+          <li>The knockout draw will take place at the college in front of everyone.</li><br />
+          <li>In the knockout round, each team has to play one match with their opponent team. In case of same points, the team with the most aggregate goals will be advanced to the next round. If the aggregate goals of both teams are the same, then another match of 6 minutes with ET & PK on will have to be played as a decider.</li><br />
+          <li>All the matches have one match official admin. If you doubt their decision, contact the main admins of the tournament.</li><br />
+          <li>6 minutes is the default match time. If any player increases the time to more than 6 minutes, then the opponent will receive a walkover win. If wrong settings are set, inform the opponent to set the right settings and then proceed to the match.</li><br />
+          <li>Playing conditions: ensure that all the settings for the match are correct, including the right team, formation, and player selection. Misuse of this rule will result in a walkover loss.</li><br />
+          <li>Matches will take place in the college lab and will start on time.</li><br />
+          <li>Any form of misconduct or violation of the game rules will result in disqualification.</li>
+        </ol>
+
+        <br /><br />
+        <b>For any further details, check out the Brochure</b>
+      </div>
+    </div>
+  );
+}
