356259cbe66e2787309b326ac958645e40a2ed59
Nanda Lal Das <tikludas01@gmail.com>
2024-10-25 00:16:38 +0530

    fixed props having implicitly any type

--- components/merch/tshirt.tsx
+++ components/merch/tshirt.tsx
@@ -3,8 +3,10 @@
 import React from 'react';
 import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
+import { ExtendedColors, Overwrite, NodeProps, NonFunctionKeys, Vector3, Euler, Matrix4, Quaternion, Layers } from '@react-three/fiber';
+import { EventHandlers } from '@react-three/fiber/dist/declarations/src/core/events';
 
-export function Model(props) {
+export function Model(props: React.JSX.IntrinsicAttributes & Omit<ExtendedColors<Overwrite<Partial<THREE.Group<THREE.Object3DEventMap>>, NodeProps<THREE.Group<THREE.Object3DEventMap>, typeof THREE.Group>>>, NonFunctionKeys<{ position?: Vector3; up?: Vector3; scale?: Vector3; rotation?: Euler; matrix?: Matrix4; quaternion?: Quaternion; layers?: Layers; dispose?: (() => void) | null; }>> & { position?: Vector3; up?: Vector3; scale?: Vector3; rotation?: Euler; matrix?: Matrix4; quaternion?: Quaternion; layers?: Layers; dispose?: (() => void) | null; } & EventHandlers) {
   const { nodes, materials } = useGLTF('/tshirt.glb'); // Load the GLB model
 
   // Debugging: Log nodes and materials to the console
