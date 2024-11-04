1453d499ea0ee733d324376b839b88796457313a
Anurag Bhattacharjee <hamsen3880@outlook.com>
2024-11-04 11:05:27 +0530

    Tech quiz button closed

--- components/events/TechQuiz/TechQuizButton.tsx
+++ components/events/TechQuiz/TechQuizButton.tsx
@@ -1,5 +1,6 @@
 export function TechQuizButton() {
     return (
-      <a href='/register' style={{ fontFamily: 'CustomFont' }} className="text-center flex justify-center items-center py-3 px-5 rounded-full font-mono text-2xl text-white bg-gradient-to-r from-violet-800 to-indigo-700 hover:from-violet-900 hover:to-indigo-800"> Register </a> 
+      // <a href='/register' style={{ fontFamily: 'CustomFont' }} className="text-center flex justify-center items-center py-3 px-5 rounded-full font-mono text-2xl text-white bg-gradient-to-r from-violet-800 to-indigo-700 hover:from-violet-900 hover:to-indigo-800"> Register </a> 
+        <p style={{ fontFamily: 'CustomFont' }} className="text-center flex justify-center items-center py-3 px-5 rounded-full font-mono text-2xl text-red-600"> REGISTRATIONS CLOSED </p> 
     );
   };
